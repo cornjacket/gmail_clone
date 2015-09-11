4c0b2984ea0ea21fbe565c846d43530d16194b1b
David Taylor <taylor.david.ray@gmail.com>
2015-09-11 08:59:40 -0700

    read from json file and display

---
+++ js/app.js
@@ -0,0 +1,14 @@
+angular.module('EmailApp', ['ngRoute'])
+  .config(function ( $routeProvider ) {
+    'use strict';
+    // configure urls
+    $routeProvider
+      // inbox route
+      .when('/inbox', {
+        templateUrl: 'views/inbox.html',
+        controller: 'InboxCtrl',
+      })
+      .otherwise({ 
+        redirectTo: '/inbox'
+      })
+  })
